c6ee28ef4675d0141821b4c926ca315c6764f188
Prakhar <prakhargaba@gmail.com>
2024-07-13 01:44:18 +0530

    2nd commit

--- app/product/[prodId]/page.tsx
+++ app/product/[prodId]/page.tsx
@@ -10,18 +10,23 @@ type Params = {
 };
 
 type ProductData = {
-  // Define the shape of your product data based on your API response
-  _id?: string;
-  name?: string;
-  description?: string;
-  price?: number;
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  brand: string;
+  color: string;
+  fabric: string;
+  gender: string;
+  stock: number;
+  images: string[];
   [key: string]: any; // Add any other fields as needed
 };
 
 export default function Page({ params }: Params) {
   console.log(params.prodId);
 
-  const [data, setData] = useState<ProductData>({});
+  const [data, setData] = useState<ProductData | null>(null);
 
   useEffect(() => {
     async function fetchData() {
@@ -32,14 +37,18 @@ export default function Page({ params }: Params) {
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
-        const data: ProductData = await response.json();
-        setData(data);
+        const productData: ProductData = await response.json();
+        setData(productData);
       } catch (error) {
         console.error("Error fetching product data:", error);
       }
     }
     fetchData();
   }, [params.prodId]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
